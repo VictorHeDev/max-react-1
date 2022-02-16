c15f1cf492d1ac291884cd8eeb329f0cd1798a9f
Woodstock <victorhe97@gmail.com>
2022-02-16 18:55:13 -0500

    Create username and age change handlers

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styles
 import classes from './AddUser.module.css';
@@ -8,17 +8,38 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (e) => {
     e.preventDefault();
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form action='' onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input
+          id='age'
+          type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
