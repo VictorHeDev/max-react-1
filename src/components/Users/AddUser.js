c9cb2a1393d65baf82e5d896dbf738766c236c93
Woodstock <victorhe97@gmail.com>
2022-02-22 15:00:02 -0500

    Error Modal now renders upon invalid response

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,15 +11,24 @@ import ErrorModal from '../UI/ErrorModal';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
       return;
     }
 
     // force the conversion of the string enteredAge to a number
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
       return;
     }
 
@@ -39,7 +48,7 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title='An error occurred!' message='Something went wrong!' />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form action='' onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
