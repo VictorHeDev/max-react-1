ac46e102c9b2812f6e855351997f988357f3a24e
Woodstock <victorhe97@gmail.com>
2022-02-25 14:43:18 -0500

    Delete the local state for username and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -12,15 +12,14 @@ import { Wrapper } from '../Helpers/Wrapper';
 const AddUser = (props) => {
   const nameInputRef = useRef();
   const ageInputRef = useRef();
-
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    console.log(nameInputRef.current.value);
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
@@ -29,7 +28,7 @@ const AddUser = (props) => {
     }
 
     // force the conversion of the string enteredAge to a number
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
@@ -37,18 +36,7 @@ const AddUser = (props) => {
       return;
     }
 
-    props.onAddUser(enteredUsername, enteredAge);
-
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
   };
 
   const errorHandler = () => {
